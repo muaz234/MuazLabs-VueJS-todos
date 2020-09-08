af3b7bb20edb55d3ad417bb2101d842890c4f6e4
muaz234 <ahmedmuaz0152@gmail.com>
2020-09-09 03:01:28 +0800

    init todos component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
 
 export default {
   name: 'App',
+  data () {
+    return {
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: "delectus aut autem",
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "quis ut nam facilis et officia qui",
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "fugiat veniam minus",
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: "et porro tempora",
+          completed: true
+        },
+      ]
+    }
+
+  },
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div id="todos" v-bind:key="todo.id" v-for="todo in todos">
+            <ul>
+             <li><span>{{todo.id}} -- <h3>{{todo.title}}</h3></span></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todos",
+        props: ["todos"]
+    }
+</script>
+
+<style scoped>
+    #todos {
+        margin: 0 auto;
+        width: 30%;
+        height: 50%;
+    }
+    h3 {
+        display: inline;
+        text-align: left !important;
+    }
+</style>
